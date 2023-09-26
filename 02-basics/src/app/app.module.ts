51c5fe15dc9c5db0d2c3fb69fbf494421856cb07
Tyler Fenton <tsfento@gmail.com>
2023-09-26 15:37:33 -0500

    Completed first assignment in Basics

---
+++ 02-basics/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessalertComponent,
+    WarningalertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

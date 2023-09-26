40c6cfb7e636a580a4567a5000cdc7f851e30c1d
Tyler Fenton <tsfento@gmail.com>
2023-09-26 15:44:01 -0500

    Update naming convention on Components

--- 02-basics/src/app/app.module.ts
+++ 02-basics/src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { SuccessalertComponent } from './successalert/successalert.component';
-import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    SuccessalertComponent,
-    WarningalertComponent
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule

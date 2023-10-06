f6bd9269901b380de577fd42733d8fe9ea079916
Tyler Fenton <tsfento@gmail.com>
2023-10-06 17:07:32 -0500

    Finished Video 8 code-along in Using Services & Dependency Injection

--- 10-using-services-and-dependency-injection/src/app/new-account/new-account.component.ts
+++ 10-using-services-and-dependency-injection/src/app/new-account/new-account.component.ts
@@ -6,13 +6,13 @@ import { AccountsService } from '../shared/accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
   constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }

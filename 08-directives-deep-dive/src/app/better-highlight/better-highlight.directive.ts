3fd5c9bf1a8c62d7990ea7e25df9a5d04d9a067e
Tyler Fenton <tsfento@gmail.com>
2023-10-06 14:15:50 -0500

    Finished Video 5 code-along in Directives Deep Dive

---
+++ 08-directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, Renderer2, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+}

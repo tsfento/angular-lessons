5a41a1dae16b8482f3dfb6fe9da1c4b5357282bb
Tyler Fenton <tsfento@gmail.com>
2023-10-06 17:35:43 -0500

    Set up for Assignment in Using Services & Dependency Injection

---
+++ 10-assignment/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

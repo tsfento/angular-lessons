ac4981309769302770a3eacee59b1447bf44fe23
Tyler Fenton <tsfento@gmail.com>
2023-10-06 14:02:15 -0500

    Finished Video 4 code-along in Directives Deep Dive

--- 08-directives-deep-dive/src/app/app.module.ts
+++ 08-directives-deep-dive/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,

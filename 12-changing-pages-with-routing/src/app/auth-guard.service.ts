cf797d3b4096bbed1c9a24a8b96c10e54fdb8448
Tyler Fenton <tsfento@gmail.com>
2023-10-12 12:52:38 -0500

    Finished Video 24 code-along in Changing Pages with Routing

--- 12-changing-pages-with-routing/src/app/auth-guard.service.ts
+++ 12-changing-pages-with-routing/src/app/auth-guard.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot,
@@ -20,4 +20,8 @@ export class AuthGuard implements CanActivate {
         }
       );
     }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(childRoute, state);
+  }
 }

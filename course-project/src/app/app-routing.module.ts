8ed6776917fe80369adf6daa42f2addcd0b8f958
Tyler Fenton <tsfento@gmail.com>
2023-10-17 10:13:15 -0500

    Finished Video 6 Challenge in Course Project - Routing

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent, children: [
+    { path: 'recipe', component: RecipeDetailComponent },
+  ]},
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 

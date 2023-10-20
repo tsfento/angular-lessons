841646d5299fa5f5ef4721344493d5ff6cc53910
Tyler Fenton <tsfento@gmail.com>
2023-10-20 14:18:51 -0500

    Finished Video 29 code-along in Handling Forms in Angular Apps

--- 16-handling-forms-in-angular-apps/src/app/app.component.ts
+++ 16-handling-forms-in-angular-apps/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +17,17 @@ export class AppComponent implements OnInit {
       'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
+      'hobbies': new FormArray([]),
     });
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control)
   }
 
   onSubmit() {

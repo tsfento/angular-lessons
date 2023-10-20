9d2db84eff4961e2d0fa99c76a752a53670d215b
Tyler Fenton <tsfento@gmail.com>
2023-10-20 15:17:02 -0500

    Starting Second Assignment in Handling Forms in Angular Apps

--- 16-assignments/src/app1/app1.component.ts
+++ 16-assignments/src/app1/app1.component.ts
@@ -1,9 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app1-root',
+  templateUrl: './app1.component.html',
+  styleUrls: ['./app1.component.css']
 })
 export class AppComponent {
   @ViewChild('f') signupForm;

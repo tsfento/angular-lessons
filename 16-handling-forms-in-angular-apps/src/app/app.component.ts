bf8c28722f03c947b56a4ecb3d8ea0fffb5a39c3
Tyler Fenton <tsfento@gmail.com>
2023-10-20 10:58:11 -0500

    Finished Video 15 code-along in Handling Forms in Angular Apps

--- 16-handling-forms-in-angular-apps/src/app/app.component.ts
+++ 16-handling-forms-in-angular-apps/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   @ViewChild('f') singupForm: NgForm;
   defaultQuestion = 'pet';
   answer: string = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';

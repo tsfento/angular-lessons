01e1ac87ee793d61d1512377e040e42305d3b436
Tyler Fenton <tsfento@gmail.com>
2023-10-27 13:22:39 -0500

    Finished Video 11 code-along in Making Http Requests

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,8 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
+
     this.http.get<{ [key: string]: Post }>(
       'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json'
       ).pipe(map((responseData) => {
@@ -48,6 +51,7 @@ export class AppComponent implements OnInit {
         }
         return postsArray;
       })).subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }

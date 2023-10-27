e9366c70f54678301d482c829cd90b8cc17f396a
Tyler Fenton <tsfento@gmail.com>
2023-10-27 15:18:07 -0500

    Finished Video 21 code-along in Making Http Requests

--- 18-making-http-requests/src/app/posts.service.ts
+++ 18-making-http-requests/src/app/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { Subject, map, catchError } from "rxjs";
+import { Subject, map, catchError, tap } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -14,7 +14,10 @@ export class PostsService {
 
     this.http.post<{ name: string }>(
       'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json',
-      postData
+      postData,
+      {
+        observe: 'response'
+      }
       ).subscribe(responseData => {
         console.log(responseData);
       }, error => {
@@ -51,7 +54,17 @@ export class PostsService {
 
   deletePosts() {
     return this.http.delete(
-      'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json'
-      );
+      'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }
+    ).pipe(tap(event => {
+      if (event.type === HttpEventType.Sent) {
+        console.log(event);
+      }
+      if (event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }));
   }
 }

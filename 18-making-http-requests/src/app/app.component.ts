6d20bf4e9754b51965607e510fe4cdb52310853d
Tyler Fenton <tsfento@gmail.com>
2023-10-27 12:55:51 -0500

    Finished Video 9 in Making Http Requests

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +17,9 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post(
+    this.http.post<{ name: string }>(
       'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json',
       postData
       ).subscribe(responseData => {
@@ -36,10 +37,10 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get(
+    this.http.get<{ [key: string]: Post }>(
       'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json'
-      ).pipe(map(responseData => {
-        const postsArray = [];
+      ).pipe(map((responseData) => {
+        const postsArray: Post[] = [];
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
             postsArray.push({ ...responseData[key], id: key });

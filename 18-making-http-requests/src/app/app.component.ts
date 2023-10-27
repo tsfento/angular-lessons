1945a6d847361d4ff96ec4f10b5ce6fe8607ba7e
Tyler Fenton <tsfento@gmail.com>
2023-10-27 13:17:11 -0500

    Finished Video 10 code-along in Making Http Requests

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -48,7 +48,7 @@ export class AppComponent implements OnInit {
         }
         return postsArray;
       })).subscribe(posts => {
-        console.log(posts);
+        this.loadedPosts = posts;
       });
   }
 }

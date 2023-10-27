5d62b30a9c04882aa3ac37b6135bf37bad4d1b0f
Tyler Fenton <tsfento@gmail.com>
2023-10-27 15:52:30 -0500

    Finished Video 25 code-along in Making Http Requests

--- 18-making-http-requests/src/app/auth-interceptor.service.ts
+++ 18-making-http-requests/src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
+import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -9,6 +10,12 @@ export class AuthInterceptorService implements HttpInterceptor {
         headers: req.headers.append('Auth', 'xyz')
       }
     );
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(tap(event => {
+      console.log(event);
+      if (event.type === HttpEventType.Response) {
+        console.log('Response arrived, body data: ');
+        console.log(event.body);
+      }
+    }));
   }
 }

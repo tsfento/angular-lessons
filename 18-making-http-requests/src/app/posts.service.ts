647dfa0d7ea3cab5093bd4a587d4aaf91c459c1d
Tyler Fenton <tsfento@gmail.com>
2023-10-27 14:58:17 -0500

    Finished Video 20 code-along in Making Http Requests

--- 18-making-http-requests/src/app/posts.service.ts
+++ 18-making-http-requests/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { Subject, map, catchError } from "rxjs";
@@ -23,10 +23,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+
     return this.http.get<{ [key: string]: Post }>(
       'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json',
       {
-        headers: new HttpHeaders({'Custom-Header': 'Hello'})
+        headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+        params: searchParams
       }
       ).pipe(map((responseData) => {
         const postsArray: Post[] = [];

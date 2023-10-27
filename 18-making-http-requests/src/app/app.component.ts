c3df64b52c29cded86efbdcd01de72ed29eae388
Tyler Fenton <tsfento@gmail.com>
2023-10-27 14:11:48 -0500

    Finished Video 15 code-along in Making Http Requests

--- 18-making-http-requests/src/app/app.component.ts
+++ 18-making-http-requests/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
@@ -20,6 +21,8 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe( posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
     });
   }
 
@@ -34,6 +37,8 @@ export class AppComponent implements OnInit {
     this.postsService.fetchPosts().subscribe( posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
     });
   }
 

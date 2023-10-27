f391c48a0cf4345b4bcb9ef6b6ee6645081ea30e
Tyler Fenton <tsfento@gmail.com>
2023-10-27 13:33:41 -0500

    Finished Video 12 code-along in Making Http Requests

---
+++ 18-making-http-requests/src/app/posts.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = {title: title, content: content};
+
+    this.http.post<{ name: string }>(
+      'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json',
+      postData
+      ).subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    this.http.get<{ [key: string]: Post }>(
+      'https://ng-complete-guide-ba783-default-rtdb.firebaseio.com/posts.json'
+      ).pipe(map((responseData) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      })).subscribe(posts => {
+        return posts;
+      });
+  }
+}

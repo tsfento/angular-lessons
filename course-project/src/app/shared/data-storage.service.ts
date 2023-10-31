b2ddc670f16ebfd01a82b7aa66789a0a88911507
Tyler Fenton <tsfento@gmail.com>
2023-10-31 10:27:13 -0500

    Finished Video 4 code-along in Course Project - Http

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -1,9 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipesService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+
+    this.http.put(
+      'https://ng-course-recipe-book-fa425-default-rtdb.firebaseio.com/recipes.json',
+      recipes).subscribe(response => {
+        console.log(response);
+      });
+  }
 }

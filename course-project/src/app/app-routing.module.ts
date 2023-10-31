ddea4b00ee04ef8041771aab616f1d1a83e4ac18
Tyler Fenton <tsfento@gmail.com>
2023-10-31 11:29:53 -0500

    Finished Video 7 code-along in Course Project - Http

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from "@angular/core";
+import { NgModule, inject } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { DataStorageService } from "./shared/data-storage.service";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent, resolve: { data: () => inject(DataStorageService).fetchRecipes() } },
+    { path: ':id/edit', component: RecipeEditComponent, resolve: { data: () => inject(DataStorageService).fetchRecipes() } },
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];

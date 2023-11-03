85f36ea192f3b1af94d3104eda082222a1d006f3
Tyler Fenton <tsfento@gmail.com>
2023-11-03 11:13:59 -0500

    Finished Video 11 code-along in Authentication & Route Protection in Angular

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -2,23 +2,26 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
 
-interface AuthResponseData {
-  kind: string;
+export interface AuthResponseData {
+  kind?: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  apiKey: string = '';
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=', {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey, {
       email: email,
       password: password,
       returnSecureToken: true,
@@ -34,5 +37,13 @@ export class AuthService {
       }
       return throwError(() => errorMessage);
     }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.apiKey, {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    });
   }
 }

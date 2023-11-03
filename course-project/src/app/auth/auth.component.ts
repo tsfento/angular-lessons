4a874af244cc02442ad603d50ee2237827ee1bf8
Tyler Fenton <tsfento@gmail.com>
2023-11-03 10:40:14 -0500

    Finished Video 9 code-along in Authentication & Route Protection in Angular

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from "./auth.service";
 })
 export class AuthComponent {
   isLoginMode: boolean = true;
+  isLoading: boolean = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
 
@@ -22,13 +24,18 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
+
     if(this.isLoginMode) {
       //...
     } else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData);
+        this.isLoading = false;
       }, error => {
         console.log(error);
+        this.error = 'An error occurred!';
+        this.isLoading = false;
       });
     }
 

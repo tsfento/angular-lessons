7f04c0a796b43420869b48591ebb7a8bd1e71ee3
Tyler Fenton <tsfento@gmail.com>
2023-11-03 11:45:58 -0500

    Finished Video 13 code-along in Authentication & Route Protection in Angular

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, throwError } from "rxjs";
+import { Subject, catchError, tap, throwError } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   kind?: string;
@@ -16,6 +17,7 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
+  user = new Subject<User>();
   apiKey: string = '';
 
   constructor(private http: HttpClient) {}
@@ -25,7 +27,9 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true,
-    }).pipe(catchError(this.handleError));
+    }).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    }));
   }
 
   login(email: string, password: string) {
@@ -33,7 +37,15 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true,
-    }).pipe(catchError(this.handleError));
+    }).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    }));
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse) {

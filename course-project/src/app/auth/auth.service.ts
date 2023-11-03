ef4f6638d00eab51d3dec6af08acb1d1720ffc1b
Tyler Fenton <tsfento@gmail.com>
2023-11-03 11:01:16 -0500

    Finished Video 10 code-along in Authentication & Route Protection in Angular

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, throwError } from "rxjs";
 
 interface AuthResponseData {
   kind: string;
@@ -21,6 +22,17 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true,
-    });
+    }).pipe(catchError(errorRes => {
+      let errorMessage = 'Unknown error occurred!'
+
+      if(!errorRes.error || !errorRes.error.error) {
+        return throwError(() => errorMessage);
+      }
+      switch(errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'E-mail already exists.'
+      }
+      return throwError(() => errorMessage);
+    }));
   }
 }

e9988fbd84af3045d32e17e4854fd413484ba5b5
Tyler Fenton <tsfento@gmail.com>
2023-11-07 12:09:49 -0600

    Finished Video 8 code-along in Angular Modules & Optimizing Angular Apps

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropDownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,12 +15,12 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,ShoppingListComponent,
-    ShoppingEditComponent,
+    HeaderComponent,
     DropDownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -35,7 +33,8 @@ import { RecipesModule } from './recipes/recipes.module';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]

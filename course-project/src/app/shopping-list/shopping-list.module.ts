09f53a744f34fd84aeaad5dbec5431cf94fcb156
Tyler Fenton <tsfento@gmail.com>
2023-11-07 14:14:08 -0600

    Finished Video 14 code-along in Angular Modules & Optimizing Angular Apps

--- course-project/src/app/shopping-list/shopping-list.module.ts
+++ course-project/src/app/shopping-list/shopping-list.module.ts
@@ -15,7 +15,7 @@ import { SharedModule } from "../shared/shared.module";
     FormsModule,
     SharedModule,
     RouterModule.forChild([
-      { path: 'shopping-list', component: ShoppingListComponent }
+      { path: '', component: ShoppingListComponent }
     ])
   ]
 })

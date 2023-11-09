29a1a5039143597f745014e7256f17384ed49c94
Tyler Fenton <tsfento@gmail.com>
2023-11-09 11:33:53 -0600

    Finished Video 10 code-along in Angular Animations

--- 19-angular-animations/src/app/app.component.ts
+++ 19-angular-animations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -95,10 +95,15 @@ import { Component } from '@angular/core';
         ]))
       ]),
       transition('* => void', [
-        animate(300, style({
-          opacity: 0,
-          transform: 'translateX(100px)'
-        }))
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            opacity: 0,
+            transform: 'translateX(100px)'
+          }))
+        ])
       ]),
     ]),
   ]

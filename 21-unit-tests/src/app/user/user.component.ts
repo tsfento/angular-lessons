b020ea09e6f130d95960c5833182f13f2b37215e
Tyler Fenton <tsfento@gmail.com>
2023-11-14 09:39:03 -0600

    Finished Video 6 code-along in A Basic Introduction to Unit Testing in Angular Apps

--- 21-unit-tests/src/app/user/user.component.ts
+++ 21-unit-tests/src/app/user/user.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  providers: [UserService]
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   user: {name: string};
   isLoggedIn = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.user = this.userService.user;
+  }
 }

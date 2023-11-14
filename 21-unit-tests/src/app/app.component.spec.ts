d979f4ecc41c039f1e4f3eedd94da0aa0c9deb06
Tyler Fenton <tsfento@gmail.com>
2023-11-14 09:14:47 -0600

    Finished Video 5 code-along in A Basic Introduction to Unit Testing in Angular Apps

--- 21-unit-tests/src/app/app.component.spec.ts
+++ 21-unit-tests/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('21-unit-tests app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('21-unit-tests app is running!');
   });
 });
